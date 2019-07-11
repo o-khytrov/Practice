a2d898e458b27ab6f3846138b7615c494cbfc7a4
o-khytrov <o.khytrov@gmail.com>
2019-07-11 22:34:47 +0300

    CharSum

---
+++ CharSum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CharSum
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+            var s = Console.ReadLine();
+            var sum = s.ToCharArray().Sum(x => x - 96);
+            Console.WriteLine(sum);
+        }
+    }
+}
