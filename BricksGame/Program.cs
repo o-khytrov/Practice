6ef9ef66125c0c0d47c07cd935a2bcc4129f2d0e
o-khytrov <o.khytrov@gmail.com>
2019-07-11 18:58:17 +0300

    Brick Game

---
+++ BricksGame/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace BricksGame
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+            var B = Int32.Parse(Console.ReadLine());
+            var motu = "Motu";
+            var paltu = "Patlu";
+            var player = paltu;
+            int c = 1;
+            while (B > 0)
+            {
+                player = paltu;
+                B -= c;
+                if (B <= 0)
+                    break;
+
+                var briksToTake = c * 2;
+                player = motu;
+                B -= briksToTake;
+                c++;
+            }
+
+            Console.WriteLine(player);
+            Console.ReadKey();
+        }
+    }
+}
