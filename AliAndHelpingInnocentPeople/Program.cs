4aa3f04a0014dce36a8edb1978aa3faa726b4ee6
o-khytrov <o.khytrov@gmail.com>
2019-07-11 20:31:34 +0300

    Ali and Helping innocent people

---
+++ AliAndHelpingInnocentPeople/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AliAndHelpingInnocentPeople
+{
+    public static class Extensions
+    {
+        public static bool IsEven(this int n)
+        {
+            return n % 2 == 0;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+            var s = Console.ReadLine();
+            var vowels = new char[] { 'A', 'E', 'I', 'O', 'U', 'Y' };
+            bool valid = true;
+            if (s.Any(x => vowels.Contains(x)))
+            {
+                valid = false;
+            }
+            if (valid)
+            {
+                for (int i = 0; i < s.Length - 1; i++)
+                {
+                    var c = s[i];
+                    var next = s[i + 1];
+                    if (char.IsDigit(c) && char.IsDigit(next))
+                    {
+                        if (!(Int32.Parse(c.ToString()) + Int32.Parse(next.ToString())).IsEven())
+                        {
+                            valid = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            var message = valid ? "valid" : "invalid";
+
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AliAndHelpingInnocentPeople/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AliAndHelpingInnocentPeople")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AliAndHelpingInnocentPeople")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b4fc235-55ed-4eba-87c4-78f4eb12eabf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
