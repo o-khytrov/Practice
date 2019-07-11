569abe603de17e759c85e465ae6304e9f997ce0c
o-khytrov <o.khytrov@gmail.com>
2019-07-11 16:52:16 +0300

    Palindromic String

---
+++ PalindromicString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromicString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader(@"D:\Console.txt"));
+            var s = Console.ReadLine();
+            var isPalindromic = true;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != s[(s.Length - 1) - i])
+                {
+                    isPalindromic = false;
+                }
+            }
+            var message = isPalindromic ? "YES" : "NO";
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
