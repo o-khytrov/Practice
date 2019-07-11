24fd205ac70572660b60051cb075ef02cf4f8feb
o-khytrov <o.khytrov@gmail.com>
2019-07-11 22:12:36 +0300

    Aman & Mr.Sharma

---
+++ AmanAndMrSharma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AmanAndMrSharma
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+
+            var D = Int32.Parse(Console.ReadLine());
+            var t = 0;
+            for (int d = 0; d < D; d++)
+            {
+                var A = Console.ReadLine().Split(' ').Select(Int32.Parse).ToArray();
+                var r = A[0];
+                var x = A[1];
+                var distance = x * 100;
+                var circleLength = 2 * 22 * r / 7;
+                var dif = distance - circleLength;
+                if (dif > 1)
+                {
+                    t++;
+                }
+            }
+            Console.WriteLine(t);
+            Console.ReadKey();
+        }
+    }
+}
--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hello
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.SetIn(new StreamReader("Console.txt"));
 
