e8b5e78bd6216093abfb4f572de974f6ae9ed67f
o-khytrov <o.khytrov@gmail.com>
2019-07-11 22:29:21 +0300

    Ladderophilia

---
+++ Ladderophilia/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Ladderophilia
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+
+            var s = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("*   *");
+            for (int i = 0; i < s; i++)
+            {
+                Console.WriteLine("*   *");
+                Console.WriteLine("*****");
+                Console.WriteLine("*   *");
+            }
+            Console.WriteLine("*   *");
+            Console.ReadKey();
+        }
+    }
+}
