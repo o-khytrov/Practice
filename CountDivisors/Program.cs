acaec895ea27c76f27199593f671b93e116bfd92
o-khytrov <o.khytrov@gmail.com>
2019-07-11 18:16:15 +0300

    Count divisors

---
+++ CountDivisors/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CountDivisors
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+
+            var A = Console.ReadLine().Split(' ').Select(Int32.Parse).ToArray();
+            var l = A[0];
+            var r = A[1];
+            var k = A[2];
+            int count = 0;
+            for (int i = l; i <= r; i++)
+            {
+                if (i % k == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
