81b7629186d00439892ba4d7389ca376b1f0b416
o-khytrov <o.khytrov@gmail.com>
2019-07-11 21:47:53 +0300

    Hello

---
+++ Hello/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+
+            Console.WriteLine("Hello Kirti");
+        }
+    }
+}
