a4bf1f69452afffdcb3ff42e1aaea4d223d9a5ad
o-khytrov <o.khytrov@gmail.com>
2019-07-11 18:26:08 +0300

    Toggle String

---
+++ ToggleString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ToggleString
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+            var s = Console.ReadLine();
+            var a = s.ToCharArray();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (char.IsUpper(a[i]))
+                {
+                    a[i] = char.ToLower(a[i]);
+                }
+                else
+                {
+                    a[i] = char.ToUpper(a[i]);
+                }
+            }
+            s = new string(a);
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
