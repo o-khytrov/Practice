88a7770463642d353dd41883b02e73b370c20f81
o-khytrov <o.khytrov@gmail.com>
2019-07-11 18:42:26 +0300

    e-maze-in

---
+++ EmazeIn/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace EmazeIn
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+            var s = Console.ReadLine();
+            var x = 0;
+            var y = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                switch (s[i])
+                {
+                    case 'R':
+                        x++;
+                        break;
+
+                    case 'L':
+                        x--;
+                        break;
+
+                    case 'D':
+                        y--;
+                        break;
+
+                    case 'U':
+                        y++;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine($"{x} {y}");
+            Console.ReadKey();
+        }
+    }
+}
