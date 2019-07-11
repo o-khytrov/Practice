d891bb0d307f5103f64fbf9112702ee82c750b69
o-khytrov <o.khytrov@gmail.com>
2019-07-11 22:18:34 +0300

    Goki and his breakup

---
+++ GokiAndHisBreakup/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace GokiAndHisBreakup
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+
+            var N = Int32.Parse(Console.ReadLine());
+            var X = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                var y = Int32.Parse(Console.ReadLine());
+                if (y >= X)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
