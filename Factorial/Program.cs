aa648beb3477c03b85a14aaf305d17618d8314b4
o-khytrov <o.khytrov@gmail.com>
2019-07-11 18:21:18 +0300

    Factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        private static int Factorial(int n)
+        {
+            var factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+            var N = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(N));
+            Console.ReadKey();
+        }
+    }
+}
