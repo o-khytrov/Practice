7d9cb36a86f0e3ceefc0992e4cb73ba783955b2d
o-khytrov <o.khytrov@gmail.com>
2019-07-11 22:25:25 +0300

    Life, the Universe, and Everything

---
+++ LifeTheUniverseAndEverything/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace LifeTheUniverseAndEverything
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+            int d = 0;
+            var process = true;
+            while (process)
+            {
+                d = Int32.Parse(Console.ReadLine());
+                if (d == 42)
+                {
+                    process = false;
+                }
+                else
+                {
+                    Console.WriteLine(d);
+                }
+            }
+        }
+    }
+}
