230252a5f8fb0bfb075f445d778bb1f989ce5f7d
o-khytrov <o.khytrov@gmail.com>
2019-07-12 13:48:05 +0300

    Cipher, Best Index

---
+++ BestIndex/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace BestIndex
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+
+            var N = Int32.Parse(Console.ReadLine());
+            var strArr = Console.ReadLine().Split(' ');
+            var A = strArr.Where(x => !string.IsNullOrWhiteSpace(x)).Select(long.Parse).ToArray();
+            for (int i = 0; i < A.Length; i++)
+            {
+                var x = (N - i) % 3;
+                for (int j = i + 1; j < N - x; j++)
+                {
+                    A[i] += A[j];
+                }
+            }
+
+            long bestIndex = 0;
+            for (int i = 1; i < A.Length; i++)
+            {
+                long sum = A[i];
+
+                if (sum > bestIndex)
+                {
+                    bestIndex = sum;
+                }
+            }
+            Console.WriteLine(bestIndex);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cipher/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace Cipher
+{
+    internal class Program
+    {
+        //A becomes E, Y becomes C, 9 becomes 3,
+        private static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+
+            var s = Console.ReadLine();
+            var key = Int32.Parse(Console.ReadLine());
+            var a = s.ToCharArray();
+            for (int i = 0; i < a.Length; i++)
+            {
+                var c = a[i];
+                if (char.IsDigit(c) || char.IsLetter(c))
+                {
+                    if (c == 'A')
+                    {
+                        a[i] = 'E';
+                    }
+                    else if (c == 'Y')
+                    {
+                        a[i] = 'C';
+                    }
+                    else if (c == '9')
+                    {
+                        a[i] = '3';
+                    }
+                    else
+                    {
+                        var offset = key;
+                        if (char.IsLetter(c) && offset > 26)
+                        {
+                            offset = offset % 26;
+                        }
+                        if (char.IsDigit(c) && offset > 10)
+                        {
+                            offset = offset % 10;
+                        }
+                        a[i] = Convert.ToChar((int)c + offset);
+                    }
+                }
+            }
+            s = new string(a);
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
