4b5971a7010732033597a0a0e02d285c8b5c3373
o-khytrov <o.khytrov@gmail.com>
2019-07-12 21:51:28 +0300

    Magical Word

---
+++ MagicalWord/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicalWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeNumbers = new int[] { 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113 };
+            Console.SetIn(new StreamReader("Console.txt"));
+            var T = Int32.Parse(Console.ReadLine());
+            for (int t = 0; t < T; t++)
+            {
+                var N = Int32.Parse(Console.ReadLine());
+                var word = Console.ReadLine();
+                var array = word.ToCharArray();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (primeNumbers.Contains(array[i]))
+                    {
+
+                    }
+                    var mag = primeNumbers.OrderBy(x => Math.Abs(x - array[i])).First();
+                    array[i] = (char)mag;
+
+                }
+                word = new string(array);
+                Console.WriteLine(word);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MinimizeCost/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimizeCost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+            var a = Console.ReadLine().Split(' ').Select(Int32.Parse).ToArray();
+
+            var N = a[0];
+            var K = a[1];
+
+            var A = Console.ReadLine().Split(' ').Select(Int32.Parse).ToArray();
+            
+
+        }
+    }
+    public static class ArrayExtensions
+    {
+        public static void Fill<T>(this T[] originalArray, T with)
+        {
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                originalArray[i] = with;
+
+            }
+        }
+
+        public static int Cost(this int[] array, int[] transferArray)
+        {
+            var sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += (array[i] + transferArray[i]);
+            }
+            return sum;
+        }
+
+    }
+}
