6b603363f0591242ef08b0078a9ef738f2896f30
o-khytrov <o.khytrov@gmail.com>
2019-07-16 16:07:22 +0300

    Micro and Array Update

---
+++ MicroAndArrayUpdate/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace MicroAndArrayUpdate
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader("Console.txt"));
+
+            var T = Int32.Parse(Console.ReadLine().Trim());
+            for (int t = 0; t < T; t++)
+            {
+                var lA = Console.ReadLine().Trim()
+                    .Split(' ')
+                    .Select(Int32.Parse)
+                    .ToArray();
+
+                var N = lA[0];
+                var K = lA[1];
+
+                var A = Console.ReadLine().Trim().Split(' ').Select(Int32.Parse).ToArray();
+                var counter = 0;
+                for (int i = 0; i < A.Length; i++)
+                {
+                    var val = A[i];
+                    if (val < K)
+                    {
+                        var d = K - val;
+                        if (d > counter)
+                        {
+                            counter = d;
+                        }
+                    }
+                }
+                Console.WriteLine(counter);
+            }
+            Console.ReadKey();
+        }
+    }
+}
