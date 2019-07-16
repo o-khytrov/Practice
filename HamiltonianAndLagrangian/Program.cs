631252f9e768a859b4362ec2e04f136c3724db7b
o-khytrov <o.khytrov@gmail.com>
2019-07-16 16:36:08 +0300

    HamiltonianAndLagrangian

---
+++ HamiltonianAndLagrangian/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HamiltonianAndLagrangian
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var value = Environment.GetEnvironmentVariable("Console_Txt", EnvironmentVariableTarget.User);
+            if (value == "True")
+                Console.SetIn(new StreamReader("Console.txt"));
+
+            var N = Int32.Parse(Console.ReadLine().Trim());
+
+            var A = Console.ReadLine().Trim().Split(' ').Select(Int32.Parse).ToArray();
+
+            var r = new List<int>();
+            for (int i = 0; i < A.Length; i++)
+            {
+                var right = i < A.Length-1 ? A[i + 1] : 0;
+                if (A[i] > right)
+                {
+                    r.Add(A[i]);
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", r));
+            Console.ReadKey();
+        }
+    }
+}
