affd86406b5072f5348273002385c1844784e146
o-khytrov <o.khytrov@gmail.com>
2019-07-17 13:27:44 +0300

    Polygon Possibility

--- ChargedUpArray/Program.cs
+++ ChargedUpArray/Program.cs
@@ -7,37 +7,72 @@ namespace ChargedUpArray
 {
     internal class Program
     {
-        
+        private static List<List<Int64>> GetSubsets(Int64[] source)
+        {
+            var dict = source.ToDictionary(x => x, x => 0);
+
+            var length = (Int64)Math.Pow(2, source.Length);
+            var result = new List<List<Int64>>();
+
+            for (int i = 0; i < length; i++)
+            {
+                var combination = new List<Int64>();
+                for (int j = 0; j < source.Length; j++)
+                {
+                    if ((i & (1 << (source.Length - j - 1))) != 0)
+                    {
+                        combination.Add(source[j]);
+                        dict[source[j]]++;
+                    }
+                }
+                result.Add(combination);
+            }
+
+            return result;
+        }
+
         private static void Main(string[] args)
         {
             var value = Environment.GetEnvironmentVariable("Console_Txt", EnvironmentVariableTarget.User);
             if (value == "True")
                 Console.SetIn(new StreamReader("Console.txt"));
 
-            var T = Int32.Parse(Console.ReadLine().Trim());
+            var T = Int64.Parse(Console.ReadLine().Trim());
 
             for (int t = 0; t < T; t++)
             {
-                var N = Int32.Parse(Console.ReadLine().Trim());
+                var N = Int64.Parse(Console.ReadLine().Trim());
 
-                var A = Console.ReadLine().Trim().Split(' ').Select(Int32.Parse).ToArray();
+                var A = Console.ReadLine().Trim().Split(' ').Select(Int64.Parse).ToArray();
+                Int64 counter = 0;
+                var dict = A.Distinct().ToDictionary(x => x, x => 0);
 
-                var subsets = new List<List<int>>();
+                var length = (Int64)Math.Pow(2, A.Length);
+                var result = new List<List<Int64>>();
+
+                for (int i = 0; i < length; i++)
+                {
+                    var combination = new List<Int64>();
+                    for (int j = 0; j < A.Length; j++)
+                    {
+                        if ((i & (1 << (A.Length - j - 1))) != 0)
+                        {
+                            combination.Add(A[j]);
+                            dict[A[j]]++;
+                        }
+                    }
+                    result.Add(combination);
+                }
                 for (int i = 0; i < A.Length; i++)
                 {
-                    subsets.Add(new List<int> { A[i] });
-                    var sublist = new List<int> { A[i] };
-
-                    for (int j = i + 1; j < A.Length; j++)
+                    var el = A[i];
+                    var count = dict[el];
+                    if (el >= count)
                     {
-                        if (i != j)
-                        {
-                            sublist.Add(A[j]);
-                        }
-                        subsets.Add(sublist);
+                        counter += el;
                     }
                 }
-
+                Console.WriteLine(counter);
             }
 
             Console.ReadKey();
