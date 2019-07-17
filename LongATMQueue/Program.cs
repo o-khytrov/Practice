9077bbb6c37e39ff978d2524b085f94ec72685c4
o-khytrov <o.khytrov@gmail.com>
2019-07-17 21:19:51 +0300

    Long ATM Queue

---
+++ LongATMQueue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LongATMQueue
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var value = Environment.GetEnvironmentVariable("Console_Txt", EnvironmentVariableTarget.User);
+            if (value == "True")
+                Console.SetIn(new StreamReader("Console.txt"));
+
+            var N = Int32.Parse(Console.ReadLine().Trim());
+
+            var A = Console.ReadLine().Trim().Split(' ').Select(Int32.Parse).ToArray();
+            int couter = 1;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (i > 0)
+                {
+                    if (A[i] < A[i - 1])
+                    {
+                        couter++;
+                    }
+                }
+            }
+            Console.WriteLine(couter);
+            Console.ReadKey();
+        }
+    }
+}
