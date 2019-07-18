a0a0dd48a6b3f5d72d5a3833ba1f2ec0c1da59b7
o-khytrov <o.khytrov@gmail.com>
2019-07-18 16:55:04 +0300

    Pepper and Contiguous Even Subarray <Debugging>

---
+++ DataStructures/Arrays/1D/PepperAndContiguousEvenSubarray/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace PepperAndContiguousEvenSubarray
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var value = Environment.GetEnvironmentVariable("Console_Txt", EnvironmentVariableTarget.User);
+            if (value == "True")
+                Console.SetIn(new StreamReader("Console.txt"));
+
+            var T = Int32.Parse(Console.ReadLine().Trim());
+
+            for (int t = 0; t < T; t++)
+            {
+                var N = Int32.Parse(Console.ReadLine().Trim());
+
+                var A = Console.ReadLine().Trim().Split(' ').Select(Int32.Parse).ToArray();
+
+                int max = 0;
+                int couter = 0;
+                for (int i = 0; i < A.Length; i++)
+                {
+                    if (A[i] % 2 == 0)
+                    {
+                        couter++;
+                    }
+                    else
+                    {
+                        if (max < couter)
+                        {
+                            max = couter;
+                        }
+                        couter = 0;
+                    }
+                    if (max < couter)
+                    {
+                        max = couter;
+                    }
+                }
+                if (max==0)
+                {
+                    max = -1;
+                }
+                Console.WriteLine(max);
+            }
+            Console.ReadKey();
+        }
+    }
+}
