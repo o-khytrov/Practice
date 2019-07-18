899f746d7f44eb32128d77299530d1de48223fcc
o-khytrov <o.khytrov@gmail.com>
2019-07-18 12:56:05 +0300

    Maximize the earning

---
+++ DataStructures/Arrays/1D/MaximizeTheEarning/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace MaximizeTheEarning
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var value = Environment.GetEnvironmentVariable("Console_Txt", EnvironmentVariableTarget.User);
+            if (value == "True")
+                Console.SetIn(new StreamReader("Console.txt"));
+
+            var S = Int32.Parse(Console.ReadLine().Trim()); // number o streets
+            for (int i = 0; i < S; i++)
+            {
+                var A = Console.ReadLine().Trim().Split(' ').Select(Int32.Parse).ToArray();
+                var N = A[0];// number of buildings
+                var R = A[1];// earning
+                A = Console.ReadLine().Trim().Split(' ').Select(Int32.Parse).ToArray();
+                int couter = R;
+                var max = A[0];
+                for (int b = 1; b < A.Length; b++)
+                {
+                    if (A[b] > max)
+                    {
+                        max = A[b];
+                        couter += R;
+                    }
+                }
+                Console.WriteLine(couter);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
