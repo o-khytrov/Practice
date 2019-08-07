563f90f61598b7e2ed56082de8c4756529a4c450
o-khytrov <o.khytrov@gmail.com>
2019-08-07 12:41:42 +0300

    CLR via C# interfaces

---
+++ Books/CLR/Interfaces/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Interfaces
+{
+    public interface ISumator
+    {
+        int Calculate(int a, int b);
+    }
+
+    public interface IMultiplyer
+    {
+        int Calculate(int a, int b);
+    }
+
+    public class Calculator : ISumator, IMultiplyer
+    {
+        int IMultiplyer.Calculate(int a, int b)
+        {
+            return a * b;
+        }
+
+         int ISumator.Calculate(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    public class ProCalculator : Calculator
+    {
+
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            var a = 5;
+            var b = 8;
+            Console.WriteLine((calc as IMultiplyer).Calculate(a, b));
+            Console.WriteLine((calc as ISumator).Calculate(a, b));
+
+            var proCalc = new ProCalculator();
+            Console.WriteLine((proCalc as IMultiplyer).Calculate(a, b));
+            Console.WriteLine((proCalc as ISumator).Calculate(a, b));
+
+            Console.ReadKey();
+        }
+    }
+}
