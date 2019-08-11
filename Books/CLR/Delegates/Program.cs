03e57eeaf0e9bd213643886732851930b8cc09a4
o-khytrov <o.khytrov@gmail.com>
2019-08-11 15:10:51 +0300

    CLR Via C# delegates

---
+++ Books/CLR/Delegates/Program.cs
@@ -0,0 +1,14 @@
+﻿using static AnonymousMethods;
+
+public sealed class Program {
+   public static void Main() {
+
+        //DelegateIntro.Go();
+        //GetInvocationList.Go();
+
+        DelegateReflection.Go("TwoInt32s", "Add", "123", "321");
+        DelegateReflection.Go("TwoInt32s", "Subtract", "123", "321");
+        DelegateReflection.Go("OneString", "NumChars", "Hello there");
+        DelegateReflection.Go("OneString", "Reverse", "Hello there");
+    }
+}
