696b2cbbad2ef4b9fb90ff10e08fad7f64640030
o-khytrov <o.khytrov@gmail.com>
2019-08-11 08:55:08 +0300

    Operator Overload Methods, Object Equality and Identity practice

---
+++ Books/CLR/Еquality/Profit.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Еquality
+{
+    public struct Profit : IComparable, IComparable<Profit>, IEquatable<Profit>
+    {
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            return CompareTo(obj);
+        }
+
+        public int CompareTo(Profit other)
+        {
+            if (other.Amount == this.Amount)
+                return 0;
+            if (other.Amount > this.Amount)
+                return -1;
+            else
+                return 1;
+        }
+
+        public bool Equals(Profit other)
+        {
+            return (CompareTo(other) == 0);
+        }
+
+        public static bool operator >(Profit p1, Profit p2)
+        {
+
+            return (p1.CompareTo(p2) > 0);
+        }
+        public static bool operator <(Profit p1, Profit p2)
+        {
+
+            return ((p1.CompareTo(p2))<0);
+        }
+        public static bool operator >=(Profit p1, Profit p2)
+        {
+
+            return p1.CompareTo(p2)<=0;
+        }
+        public static bool operator <=(Profit p1, Profit p2)
+        {
+
+            return p1.CompareTo(p2)>=0;
+        }
+
+        public static bool operator ==(Profit p1, Profit p2)
+        {
+
+            return (p1.CompareTo(p2)==0);
+        }
+        public static bool operator !=(Profit p1, Profit p2)
+        {
+
+            return (p1.CompareTo(p2) != 0);
+        }
+    }
+}
---
+++ Books/CLR/Еquality/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Еquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Profit() { Amount = 1000, Description = "Q1" };
+            var p2 = new Profit() { Amount = 1000, Description = "Q2" };
+
+            Console.WriteLine(p1.CompareTo(p2));
+            Console.WriteLine(p1 > p2);
+            Console.WriteLine(p2 < p1);
+            Console.WriteLine(p2 <= p1);
+            Console.ReadKey();
+        }
+
+    }
+}
