c00d4f4f253bafa97a053cafdd59586133c083fc
o-khytrov <o.khytrov@gmail.com>
2019-08-15 09:06:52 +0300

    ITVDN Collections

---
+++ Books/ITVDN/Collections/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var col = new UserCollection();
+            foreach (var item in col)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------------");
+            foreach (var item in col)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in col)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------------");
+            foreach (var item in col)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Books/ITVDN/Collections/UserCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class UserCollection : IEnumerator, IEnumerable
+    {
+        private string[] ar = new string[] { "John", "Mary", "Bob", "Albert" };
+
+        int position = -1;
+
+        public object Current => ar[position];
+
+        public IEnumerator GetEnumerator()
+        {
+            return this as IEnumerator;
+        }
+
+        public bool MoveNext()
+        {
+            if (position < ar.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            else
+            {
+                Reset();
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
