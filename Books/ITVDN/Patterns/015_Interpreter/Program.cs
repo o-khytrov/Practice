bdc06c552d48baf35c1d4543ada47184f7e53183
o-khytrov <o.khytrov@gmail.com>
2019-09-10 09:11:26 +0300

    Iterator, Interpreter

---
+++ Books/ITVDN/Patterns/015_Interpreter/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _015_Interpreter
+{
+    abstract class AbstractExpression
+    {
+        public abstract void Interpret(Context context);
+    }
+    class TerminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            context.Result = context.Source[context.Position] == context.Vocabulary;
+        }
+    }
+
+    class NonTerminalExpression : AbstractExpression
+    {
+        AbstractExpression nonTerminalExpression;
+        AbstractExpression terminalExpression;
+
+        public override void Interpret(Context context)
+        {
+            if (context.Position < context.Source.Length)
+            {
+                terminalExpression = new TerminalExpression();
+                terminalExpression.Interpret(context);
+                context.Position++;
+                if (context.Result)
+                {
+                    nonTerminalExpression = new NonTerminalExpression();
+                    nonTerminalExpression.Interpret(context);
+                }
+            }
+
+        }
+    }
+
+    class Context
+    {
+        public string Source { get; set; }
+        public char Vocabulary { get; set; }
+        public bool Result { get; set; }
+        public int Position { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context
+            {
+                Vocabulary = 'a',
+                Source = "aaa"
+            };
+
+            var expression = new NonTerminalExpression();
+            expression.Interpret(context);
+            Console.WriteLine(context.Result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Books/ITVDN/Patterns/016_Iterator/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _016_Iterator
+{
+    //Также известен как курсор 
+    //Представляет удобный и безопасный способ доступа к элементам коллекции (составного объекта), при этом не расскрывая внутреннего 
+    //представления этой коллекции
+
+    class Bank : IEnumerable
+    {
+        List<Baknote> bankVaul = new List<Baknote>
+        {
+            new Baknote(),new Baknote(),new Baknote(),new Baknote()
+        };
+
+        public Baknote this[int index]
+        {
+            get { return bankVaul[index]; }
+            set { bankVaul[index] = value; }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new Cashier(this);
+        }
+        public int Count { get { return bankVaul.Count; } }
+    }
+
+    internal class Cashier : IEnumerator
+    {
+        private Bank bank;
+
+        public Cashier(Bank bank)
+        {
+            this.bank = bank;
+        }
+
+        private int current = -1;
+
+        public object Current => bank[current];
+
+        public bool MoveNext()
+        {
+            if (current < bank.Count-1)
+            {
+                current++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            current = -1;
+        }
+    }
+
+    internal class Baknote
+    {
+        public string Nominal = "100 $";
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var bank = new Bank();
+            foreach (Baknote banknote in bank)
+            {
+                Console.WriteLine(banknote.Nominal);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
