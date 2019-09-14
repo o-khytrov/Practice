fd0e6343d4a4f4580a2aaabe2b282ba80020eb80
o-khytrov <o.khytrov@gmail.com>
2019-09-14 08:17:46 +0300

    Lost in strings

---
+++ Books/CLR/Threading/012_Volatile/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _012_Volatile
+{
+    internal sealed class ThreadsSharingData
+    {
+        private Int32 m_flag = 0;
+        private Int32 m_value = 0;
+        // This method is executed by one thread
+        public void Thread1()
+        {
+            // Note: These could execute in reverse order
+            m_value = 5;
+            Volatile.Write(ref m_flag, 1);
+        }
+        // This method is executed by another thread
+        public void Thread2()
+        {
+
+            // Note: m_value could be read before m_flag
+            if (Volatile.Read(ref m_flag) == 1)
+                Console.WriteLine(m_value);
+        }
+
+        static void Main()
+        {
+            var c = new ThreadsSharingData();
+            var t1 = new Thread(c.Thread1);
+            var t2 = new Thread(c.Thread2);
+            t1.Start();
+            t2.Start();
+            Console.ReadKey();
+        }
+    }
+    //internal static class StrangeBehavior
+    //{
+    //    // As you'll see later, mark this field as volatile to fix the problem
+    //    private static Boolean s_stopWorker = false;
+    //    public static void Main()
+    //    {
+    //        Console.WriteLine("Main: letting worker run for 5 seconds");
+    //        Thread t = new Thread(Worker);
+    //        t.Start();
+    //        Thread.Sleep(5000);
+    //        s_stopWorker = true;
+    //        Console.WriteLine("Main: waiting for worker to stop");
+    //        t.Join();
+    //    }
+
+    //    private static void Worker(Object o)
+    //    {
+    //        Int32 x = 0;
+    //        while (!s_stopWorker) x++;
+    //        Console.WriteLine("Worker: stopped when x={0}", x);
+    //    }
+    //}
+
+}
---
+++ Challenges/Algorithms and data structures/2/LostInStrings/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LostInStrings
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var value = Environment.GetEnvironmentVariable("Console_Txt", EnvironmentVariableTarget.User);
+            if (value == "True")
+                Console.SetIn(new StreamReader("Console.txt"));
+
+            var list = new List<string>();
+            var S = Console.ReadLine();
+            list.Add(S);
+
+            var Q = Int32.Parse(Console.ReadLine().Trim());
+            for (int i = 0; i < Q; i++)
+            {
+                var A = Console.ReadLine().Trim().Split(' ').ToArray();
+                var q = Int32.Parse(A[0]);
+                var p = Int32.Parse(A[1]);
+                var n = Int32.Parse(A[2]);
+                string c = string.Empty; ;
+                if (q != 2)
+                {
+                    c = A[3];
+                }
+
+                string newStr;
+                switch (q)
+                {
+                    case 1:
+                        newStr = list[n - 1].Substring(0, (p - 1 > list[n - 1].Length) ? list[n - 1].Length : p - 1) + c;
+                        list.Add(newStr);
+                        break;
+
+                    case 2:
+                        newStr = list[n - 1].Substring(0, (p - 1 > list[n - 1].Length) ? list[n - 1].Length : p - 1);
+                        list.Add(newStr);
+                        break;
+
+                    case 3:
+                        var found = false;
+                        for (int j = p - 1; j <= n - 1; j++)
+                        {
+                            if (list[j].StartsWith(c))
+                            {
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (found)
+                        {
+                            Console.WriteLine("yes");
+                        }
+                        else
+                        {
+                            Console.WriteLine("no");
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
